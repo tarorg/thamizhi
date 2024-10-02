1198acce15e83ec282f5b39d1e8caeaab88d6492
wetarorg <tarframework@gmail.com>
2024-10-02 22:36:27 +0530

    thamizhi2

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+});
